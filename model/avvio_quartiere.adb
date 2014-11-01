42a3388b8a58c5de81f07edbbd54b1f04a8b23d9
marcobaesso <baesso91@gmail.com>
2014-11-01 16:43:40 +0100

    reintegrato web server; (la mappa non viene disegnata)

--- model/avvio_quartiere.adb
+++ model/avvio_quartiere.adb
@@ -55,7 +55,7 @@ package body avvio_quartiere is
          File_String_IO.Read(File, Item => contents);
          File_String_IO.Close(File);
          Put_Line("Registering quartiere " & Integer'Image(get_id_quartiere));
-         --get_webServer.registra_mappa_quartiere(contents, get_id_quartiere);
+         get_webServer.registra_mappa_quartiere(contents, get_id_quartiere);
       end;
 
       registra_quartiere_entities_life(get_id_quartiere,ptr_rt_quartiere_entities_life(get_quartiere_entities_life_obj));
